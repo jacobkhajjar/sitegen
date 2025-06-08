ada0d0679ec67b8ab3b810c6496b4ccbb7f72668
jacobkhajjar <jacob.k.hajjar@gmail.com>
2025-06-08 15:15:01 +0100

    deployment 1.0

--- src/generate_pages.py
+++ src/generate_pages.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from markdown_to_html import markdown_to_html_node
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath="/"):
     files = os.listdir(dir_path_content)
     for f in files:
         dir_path = os.path.join(dir_path_content, f)
@@ -12,9 +12,9 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             print(f"- generating page {dir_path} -> {destination} using {template_path}")
             generate_page(dir_path, template_path, destination)
         elif os.path.isdir(dir_path):
-            generate_pages_recursive(dir_path, template_path, destination)
+            generate_pages_recursive(dir_path, template_path, destination, basepath)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     with open(from_path) as f:
         markdown = f.read()
 
@@ -25,6 +25,8 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", extract_title(markdown))
     template = template.replace("{{ Content }}", content)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-import os, shutil, pathlib
+import os, shutil, pathlib, sys
 
 from copy_static import copy_dir
 from generate_pages import generate_page, generate_pages_recursive
 
+basepath = sys.argv[0]
 path_static = pathlib.Path("./static")
-path_public = pathlib.Path("./public")
+path_public = pathlib.Path("./docs")
 path_content = pathlib.Path("./content")
 path_template = pathlib.Path("./template.html")
     
@@ -20,8 +21,9 @@ def main():
     copy_dir(f"{path_static}", f"{path_public}")
 
     print("(~~~~ GENERATING PAGES ~~~~)")
-    generate_pages_recursive(path_content, path_template, path_public)
+    generate_pages_recursive(path_content, path_template, path_public, basepath)
 
     print("(~~~~ PUSHING TO SERVER PORT 8888 ~~~~)")
+    print(f"basepath: {basepath}")
     
 main()
